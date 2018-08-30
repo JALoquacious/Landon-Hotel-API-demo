a631612d17a13e1659bc226bc29044ba168befec
JALoquacious <jalikoudis@protonmail.com>
2018-08-30 19:42:34 -0400

    Add root controller

---
+++ LandonApi/Controllers/RootController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LandonApi.Controllers
+{
+    [Route("/")]
+    public class RootController : Controller
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(response);
+        }
+    }
+}
