b029b05e7f3a2fdddf020ec2b75f26422d79ab2a
JALoquacious <jalikoudis@protonmail.com>
2018-09-04 13:55:39 -0400

    Install Entity Framework; add context and Room classes

---
+++ LandonApi/HotelApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LandonApi
+{
+    public class HotelApiContext : DbContext
+    {
+        public HotelApiContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<RoomEntity> Rooms { get; set; }
+    }
+}
--- LandonApi/Startup.cs
+++ LandonApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -43,6 +44,9 @@ namespace LandonApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Use in-memory database for development (swap with real database in production)
+            services.AddDbContext<HotelApiContext>(opt => opt.UseInMemoryDatabase());
+
             // Add framework services.
             services.AddMvc(opt =>
             {
