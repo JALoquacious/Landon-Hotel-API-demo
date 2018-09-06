2e58e8f2674d58f125b196227ff3a3a2a27db383
JALoquacious <jalikoudis@protonmail.com>
2018-09-06 12:34:35 -0400

    Create second set of files related to Bookings functionality

--- LandonApi/HotelApiContext.cs
+++ LandonApi/HotelApiContext.cs
@@ -9,5 +9,6 @@ namespace LandonApi
             : base(options) { }
 
         public DbSet<RoomEntity> Rooms { get; set; }
+        public DbSet<BookingEntity> Bookings { get; set; }
     }
 }
---
+++ LandonApi/Services/IOpeningService.cs
@@ -0,0 +1,19 @@
+﻿using LandonApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LandonApi.Services
+{
+    public interface IOpeningService
+    {
+        Task<IEnumerable<Opening>> GetOpeningsAsync(CancellationToken ct);
+
+        Task<IEnumerable<BookingRange>> GetConflictingSlots(
+            Guid roomId,
+            DateTimeOffset start,
+            DateTimeOffset end,
+            CancellationToken ct);
+    }
+}
