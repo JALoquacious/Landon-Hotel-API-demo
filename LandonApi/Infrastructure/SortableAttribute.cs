324651d280b8fe954d7942a82c9f01fd0873f8db
JALoquacious <jalikoudis@protonmail.com>
2018-09-07 15:36:37 -0400

    Add sort attributes

---
+++ LandonApi/Infrastructure/SortableAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LandonApi.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class SortableAttribute : Attribute
+    {
+
+    }
+}
