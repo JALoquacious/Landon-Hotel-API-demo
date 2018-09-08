bc919aeabe2e7fa8eb604ae374ab7e0c83596fbd
JALoquacious <jalikoudis@protonmail.com>
2018-09-08 12:57:02 -0400

    Build search expressions dynamically

--- LandonApi/Models/SearchOptions(T, TEntity).cs
+++ LandonApi/Models/SearchOptions(T, TEntity).cs
@@ -27,7 +27,8 @@ namespace LandonApi.Models
 
         public IQueryable<TEntity> Apply(IQueryable<TEntity> query)
         {
-            throw new NotImplementedException();
+            var processor = new SearchOptionsProcessor<T, TEntity>(Search);
+            return processor.Apply(query);
         }
     }
 }
