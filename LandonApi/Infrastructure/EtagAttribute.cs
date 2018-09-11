83c0c75f3e4fbe4ee5b09c42186ac535c137fb60
JALoquacious <jalikoudis@protonmail.com>
2018-09-10 20:46:38 -0400

    Support the ETag header

---
+++ LandonApi/Infrastructure/EtagAttribute.cs
@@ -0,0 +1,15 @@
+﻿using LandonApi.Filters;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace LandonApi.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class EtagAttribute : Attribute, IFilterFactory
+    {
+        public bool IsReusable => true;
+
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+            => new EtagHeaderFilter();
+    }
+}
